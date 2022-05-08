3fe367fa3b41b42cb7710dbd2ea96f4041618e4a
shiiman <hsnonsense5@gmail.com>
2022-05-08 18:11:56 +0900

    add Event

---
+++ event.go
@@ -0,0 +1,60 @@
+package goshopify
+
+import (
+	"fmt"
+	"time"
+)
+
+const eventBasePath = "events"
+
+// EventService is an interface for interacting with the
+// Event endpoints of the Shopify API.
+// See https://help.shopify.com/api/reference/billing/applicationevent
+type EventService interface {
+	Get(int64, interface{}) (*Event, error)
+	List(interface{}) ([]Event, error)
+}
+
+type EventServiceOp struct {
+	client *Client
+}
+
+type Event struct {
+	ID           int64      `json:"id"`
+	SubjectID    int64      `json:"subject_id"`
+	CreatedAt    *time.Time `json:"created_at"`
+	Subject_type string     `json:"subject_type"`
+	Verb         string     `json:"verb"`
+	Arguments    []string   `json:"arguments"`
+	Body         string     `json:"body"`
+	Message      string     `json:"message"`
+	Author       string     `json:"author"`
+	Description  string     `json:"description"`
+	Path         string     `json:"path"`
+}
+
+// EventResource represents the result from the
+// admin/events{/X{/activate.json}.json}.json endpoints.
+type EventResource struct {
+	Credit *Event `json:"event"`
+}
+
+// EventsResource represents the result from the
+// admin/events.json endpoint.
+type EventsResource struct {
+	Credits []Event `json:"events"`
+}
+
+// Get gets individual application event.
+func (a EventServiceOp) Get(eventID int64, options interface{}) (*Event, error) {
+	path := fmt.Sprintf("%s/%d.json", eventBasePath, eventID)
+	resource := &EventResource{}
+	return resource.Credit, a.client.Get(path, resource, options)
+}
+
+// List gets all application events.
+func (a EventServiceOp) List(options interface{}) ([]Event, error) {
+	path := fmt.Sprintf("%s.json", eventBasePath)
+	resource := &EventsResource{}
+	return resource.Credits, a.client.Get(path, resource, options)
+}
